5d7bf4caaf8eb29c2b00755b44b9de332f39c4d2
huchuchuch <34954783+huchuchuch@users.noreply.github.com>
2022-09-07 23:24:50 +0800

    new monitor tracking socket()
    filter socket with AF_NETLINK domain and NETLINK_ROUTE protocol

---
+++ neo_monitor.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+from bcc import BPF
+from bcc.containers import filter_by_containers
+from bcc.utils import ArgString, printb
+import bcc.utils as utils
+import argparse
+import re
+import time
+import pwd
+from collections import defaultdict
+from time import strftime
+
+bpf_text = """
+#include <uapi/linux/ptrace.h>
+#include <linux/sched.h>
+#include <linux/fs.h>
+
+
+struct data_t {
+	u32 pid;
+	u32 ppid;
+	int domain;
+	int protocol;
+	char comm[TASK_COMM_LEN];
+};
+
+BPF_PERF_OUTPUT(events);
+
+int syscall__socket(struct pt_regs* ctx,
+	int domain,
+	int type,
+	int protocol)
+{
+	struct data_t data = {};
+	struct task_struct * task;
+
+	data.pid = bpf_get_current_pid_tgid() >> 32;
+	task = (struct task_struct *)bpf_get_current_task();
+	data.ppid = task->real_parent->tgid;
+	bpf_get_current_comm(&data.comm, sizeof(data.comm));
+	bpf_probe_read_user(&data.domain, sizeof(data.domain), &domain);
+	bpf_probe_read_user(&data.protocol, sizeof(data.protocol), &protocol);
+	events.perf_submit(ctx, &data, sizeof(data));
+	return 0;
+}
+"""
+
+b = BPF(text = bpf_text)
+socket_fnname = b.get_syscall_fnname("socket")
+b.attach_kprobe(event=socket_fnname, fn_name="syscall__socket")
+
+print("%-12s %-10s %-10s %s" % ("TS(ns)", "PPID", "PID", "CMD"))
+
+start_ts = time.time_ns()
+
+def print_event(cpu, data, size):
+	event = b["events"].event(data)
+	if (event.domain == 16 and event.protocol == 0): # AF_NETLINK - 16 NETLINK_ROUTE - 0
+		printb(b"%-12d %-10d %-10d %s" % (time.time_ns() - start_ts, event.ppid, event.pid, event.comm))
+
+# loop with callback to print_event
+b["events"].open_perf_buffer(print_event)
+while 1:
+	try:
+		b.perf_buffer_poll()
+	except KeyboardInterrupt:
+		exit()
+
+
